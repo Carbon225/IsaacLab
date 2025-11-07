f19625434a9621cac2e22173f792e7e2590fe530
Carbon225 <carbon225@protonmail.com>
2025-11-07 18:25:20 +0100

    tune

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/piesors/agents/rsl_rl_ppo_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/piesors/agents/rsl_rl_ppo_cfg.py
@@ -11,7 +11,7 @@ from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlPpoActorCriticCfg, R
 @configclass
 class PiesorsRoughPPORunnerCfg(RslRlOnPolicyRunnerCfg):
     num_steps_per_env = 24
-    max_iterations = 1500
+    max_iterations = 2000
     save_interval = 50
     experiment_name = "piesors_rough"
     policy = RslRlPpoActorCriticCfg(
